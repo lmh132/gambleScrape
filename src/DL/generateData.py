5cb007fb9e2598c684d47e9bfb3173ca6c7e1770
Leo Hu <leo.hu@duke.edu>
2024-06-15 13:29:06 -0400

    train data

--- src/DL/generateData.py
+++ src/DL/generateData.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 import json
 import pickle
 import pprint
@@ -7,7 +7,6 @@ from sklearn.preprocessing import StandardScaler
 
 a = open("data/teamstats.json")
 b = open("data/pitcherstats2.json")
-scaler = StandardScaler()
 
 teamstats = json.load(a)
 pitcherstats = json.load(b)
@@ -35,7 +34,29 @@ for pitcher in pitcherstats:
             sos = pitchergames[pitcherkeys[i+window_size]]["SO"]
 
             prevgames += seq
-            oppstats += opponent
+            oppstats.append(opponent)
             strikeouts.append(sos)
 
-pprint.pp(prevgames[:6])
+
+
+scaler1 = StandardScaler().fit(prevgames)
+scaler2 = StandardScaler().fit(oppstats)
+
+scaled_prevgamedata = scaler1.transform(prevgames)
+scaled_oppdata = scaler2.transform(oppstats)
+
+PREV = np.array(scaled_prevgamedata).reshape(-1, 3, 8)
+OPP = np.array(scaled_oppdata)
+SO = np.array(strikeouts)
+
+with open("data/PREV.pkl", "wb") as f:
+    pickle.dump(PREV, f)
+    f.close()
+
+with open("data/OPP.pkl", "wb") as f:
+    pickle.dump(OPP, f)
+    f.close()
+
+with open("data/SO.pkl", "wb") as f:
+    pickle.dump(SO, f)
+    f.close()
